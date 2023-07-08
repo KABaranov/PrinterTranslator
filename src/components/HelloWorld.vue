483bccba8455d1c3993d6ee660d4422d9a543929
KABaranov <1arentir1@gmail.com>
2023-07-08 13:14:16 +0500

    new file:   .browserslistrc new file:   .editorconfig new file:   .gitignore new file:   index.html new file:   package-lock.json new file:   package.json new file:   public/favicon.ico new file:   public/xweld.png new file:   src/App.vue new file:   src/assets/logo.png new file:   src/assets/logo.svg new file:   src/components/HelloWorld.vue new file:   src/main.ts new file:   src/plugins/index.ts new file:   src/plugins/vuetify.ts new file:   src/plugins/webfontloader.ts new file:   src/vite-env.d.ts new file:   tsconfig.json new file:   tsconfig.node.json new file:   vite.config.ts

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="d-flex flex-row" :style="{backgroundColor: color3}">
+    <!-- SideBar -->
+    <div 
+    class="h-screen d-flex flex-column justify-space-around" :style="{backgroundColor: color1}" style="width: 13%;">
+      <div>
+        <button class="w-100 my-3" style="padding-top: 10%; padding-bottom: 10%;" :style="{backgroundColor: color2, color: color4}">Kuka</button>
+        <button class="w-100 my-3" style="padding-top: 10%; padding-bottom: 10%;" :style="{backgroundColor: color4, color: color2}">Fanuc</button>
+        <button class="w-100 my-3" style="padding-top: 10%; padding-bottom: 10%;" :style="{backgroundColor: color4, color: color2}">AT300</button>
+      </div>
+      <div class="w-100 d-flex justify-center">
+        <img class="w-50" src="/xweld.png" alt="XWeld">
+      </div>
+    </div>
+    <!-- Main Part -->
+    <div class="w-100 my-6" :style="{backgroundColor: color2}">
+      <v-col justify="start">
+        <!-- Input Files -->
+        <div class="rounded-lg w-25 pa-5" style="height: 10%;" :style="{backgroundColor: color3}">
+          <v-file-input clearable label="Input File"></v-file-input>
+          <v-file-input clearable label="Output File" disabled></v-file-input>
+        </div>
+      </v-col>
+    </div>
+  </div>
+</template>
+<script>
+  export default{
+    data() {
+      return {
+        color1: '#EEC170',
+        color2: '#F2A65A',
+        color3: '#F58549',
+        color4: '#772F1A',
+        color5: '#585123',
+      }
+    },
+  }
+</script>
