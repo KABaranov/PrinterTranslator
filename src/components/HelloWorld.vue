064544164da477b71a14067ffa8ded6b327bdba6
KABaranov <1arentir1@gmail.com>
2023-07-08 15:35:55 +0500

    Инпуты

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,11 +16,43 @@
     <div class="w-100 my-6" :style="{backgroundColor: color2}">
       <v-col justify="start">
         <!-- Input Files -->
-        <div class="rounded-lg w-25 pa-5" style="height: 10%;" :style="{backgroundColor: color3}">
+        <div class="rounded-lg pa-2" :style="{backgroundColor: color3}" style="width: 33%">
           <v-file-input clearable label="Input File"></v-file-input>
           <v-file-input clearable label="Output File" disabled></v-file-input>
         </div>
       </v-col>
+      <v-row class="mx-2 mt-1">
+        <v-col align="center" class="rounded-lg mx-1 w-25" :style="{backgroundColor: color3}">
+          <div class="text-h5 my-4" :style="{color: color4}">Welding Parameters</div>
+          <v-text-field class="px-4" clearable label="Weld Feed Rate (mm/sec)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Idle Feed Rate (mm/sec)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Min Move Length (mm)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Layer Height (mm)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Forging Feed Rate (mm/sec)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Forging Layer Height (mm)"></v-text-field>
+        </v-col>
+        <v-col align="center" class="rounded-lg mx-1 w-25" :style="{backgroundColor: color3}">
+          <div class="text-h5 my-4" :style="{color: color4}">Oscillation Parameters</div>
+          <v-select
+            clearable
+            class="px-4"
+            label="Select"
+            :items="['Вариант 1', 'Вариант 2', 'Вариант 3']"
+          ></v-select>
+          <v-text-field class="px-4" clearable label="X Amplitude (mm)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Y Amplitude (mm)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Discretization (pcs)"></v-text-field>
+        </v-col>
+        <v-col align="center" class="rounded-lg mx-1 w-25" :style="{backgroundColor: color3}">
+          <div class="text-h5 my-4" :style="{color: color4}">Initialization Parameters</div>
+          <v-text-field class="px-4" clearable label="Safe Z Position (mm)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Time Between Layers (mm/sec)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Min Move Length (mm)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Layer Height (mm)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Forging Feed Rate (mm/sec)"></v-text-field>
+          <v-text-field class="px-4" clearable label="Forging Layer Height (mm)"></v-text-field>
+        </v-col>
+      </v-row>
     </div>
   </div>
 </template>
